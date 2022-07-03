dfffab5ba48e34a4598eff1d2afe73a6a0a06eec
arnab0308 <69678268+arnab0308@users.noreply.github.com>
2022-07-03 09:45:28 +0530

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing 2nd time...");
 		assertEquals(true, true);
 	}
 
